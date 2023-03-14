cae8d8c75d343c84230f1dac433be02420d72aa8
Armando Capriles <armandotechno12@gmail.com>
2023-03-14 13:19:29 -0400

    Componente FlatList

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,38 @@
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
+
+interface MenuItem {
+    name: string;
+    icon: string;
+    components: string;
+}
+
+const menuItems = [
+    {
+        name: 'Animation 101',
+        icon: 'cube-outline',
+        components: 'Animation101Screen'
+    }
+]
 
 export const HomeScreen = () => {
+
+    const renderMenuItem = ( menuItem: MenuItem ) => {
+        return (
+            <View>
+                <Text>{ menuItem.name } - { menuItem.icon }</Text>
+            </View>
+        )
+    }
+
     return (
-      <View>
-        <Text>Home Screen</Text>
+      <View style={{ flex: 1 }}>
+        
+        <FlatList 
+            data={ menuItems }
+            renderItem={ ({ item }) => renderMenuItem( item ) }
+            keyExtractor={ (item) => item.name }
+        />
+
       </View>
     )
 }
