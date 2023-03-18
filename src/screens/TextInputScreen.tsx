0926a8fd9c7f66b065bb0de801923826cc12076b
Armando Capriles <armandotechno12@gmail.com>
2023-03-17 23:38:06 -0400

    Multiples TextInputs en pantalla

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,16 +1,52 @@
 import { View, Text, TextInput, StyleSheet } from 'react-native'
 import HeaderTitle from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
+import { useState } from 'react';
 
 export default function TextInputScreen() {
+
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        phone: '',
+    })
+
+    const onChange = ( value: string, field: string ) => {
+        setForm({
+            ...form,
+            [field]: value
+        })
+    }
+
     return (
       <View style={ styles.globalMargin }>
-        <HeaderTitle title='InputText' />
+        <HeaderTitle title="InputText" />
 
         <TextInput 
-            style={ stylesScreen.textInputStyle }           
+            style={ stylesScreen.textInputStyle }    
+            placeholder="Ingrese su nombre"     
+            autoCorrect={ false }  
+            autoCapitalize="words"
+            onChangeText={ (value) => onChange( value, 'name')}
+        />
+        
+        <TextInput 
+            style={ stylesScreen.textInputStyle }
+            placeholder="Ingrese su email"  
+            autoCorrect={ false }  
+            autoCapitalize="none"
+            onChangeText={ (value) => onChange( value, 'email')}
+            keyboardType="email-address"
         />
 
+        <TextInput 
+            style={ stylesScreen.textInputStyle }   
+            placeholder="Ingrese su teléfono"     
+            onChangeText={ (value) => onChange( value, 'phone')}  
+            keyboardType="phone-pad"
+        />
+
+        <HeaderTitle title={ JSON.stringify( form, null, 3 ) } />
       </View>
     )
 }
@@ -22,5 +58,6 @@ const stylesScreen = StyleSheet.create({
         height: 50,
         paddingHorizontal: 10,
         borderRadius: 10,
+        marginVertical: 10,
     },
 });
